cf9cca8510aedc46ff0b45f70ca4756e79cfe20d
Vengeance00 <vengeance801@gmail.com>
2023-01-03 17:56:40 -0600

    Primeros pasos con componentes

--- 01-BASES/src/app/app.module.ts
+++ 01-BASES/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContadorComponent } from './Contador/contador.component';
+import { HeroeComponent } from './Heroes/Heroe/heroe.component';
+import { ListadoComponent } from './Heroes/listado/listado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContadorComponent,
+    HeroeComponent,
+    ListadoComponent
   ],
   imports: [
     BrowserModule
